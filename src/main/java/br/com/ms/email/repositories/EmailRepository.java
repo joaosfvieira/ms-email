9b10896897d6df6395352b067daefe5708921f2b
João de Souza <joaosfvieira@hotmail.com>
2023-12-15 09:46:02 -0300

    complete beta project
    The base version of the project built following Michelli Brito's course on microservices.

---
+++ src/main/java/br/com/ms/email/repositories/EmailRepository.java
@@ -0,0 +1,11 @@
+package br.com.ms.email.repositories;
+
+import br.com.ms.email.models.EmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
+}
---
+++ src/main/java/br/com/ms/email/services/EmailService.java
@@ -0,0 +1,52 @@
+package br.com.ms.email.services;
+
+import br.com.ms.email.enums.StatusEmail;
+import br.com.ms.email.models.EmailModel;
+import br.com.ms.email.repositories.EmailRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class EmailService {
+    @Autowired
+    EmailRepository emailRepository;
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    @Value(value = "${spring.mail.username}")
+    private String emailFrom;
+
+    @Transactional
+    public EmailModel sendEmail(EmailModel emailModel) {
+        try {
+            emailModel.setSendDateEmail(LocalDateTime.now());
+            emailModel.setEmailFrom(emailFrom);
+
+            SimpleMailMessage message = createMessageFromEmailModel(emailModel);
+            javaMailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
+    }
+
+    public SimpleMailMessage createMessageFromEmailModel(EmailModel emailModel) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(emailModel.getEmailTo());
+        message.setSubject(emailModel.getSubject());
+        message.setText(emailModel.getText());
+
+        return message;
+    }
+}
